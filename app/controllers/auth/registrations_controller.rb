764c50594f7b2201f378fa7497b96b883bebccf0
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-02-26 09:10:58 -0300

    Implement user authentication with Devise and JWT, add user model and serializer, and configure CORS

---
+++ app/controllers/auth/registrations_controller.rb
@@ -0,0 +1,24 @@
+class Auth::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
+
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      @token = request.env["warden-jwt_auth.token"]
+      headers["Authorization"] = @token
+
+      render json: {
+        status: { code: 200, message: "Signed up successfully.",
+                  token: @token,
+                  user: UserSerializer.render_as_json(resource) }
+      }
+    else
+      render json: {
+        status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
+end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :uuid             not null, primary key
+#  cpf                    :string
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  date_of_birth          :date
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  full_name              :string
+#  jti                    :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  phone                  :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  status                 :integer
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_jti                   (jti)
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_status                (status)
+#
+class UserSerializer < ApplicationSerializer
+  identifier :id
+
+  fields :id, :email, :full_name
+end
