d4fa6ba9aec65bbf866276916f3ef87edcd6ceb2
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-04-02 23:17:54 -0300

    Update JWT expiration time from 10 seconds to 1 day for improved session management

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 10.seconds.to_i
+    jwt.expiration_time = 1.day.to_i
   end
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
