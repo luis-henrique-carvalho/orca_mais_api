67d3fbf13bfa04eec9e6741d35e810bb89db04a0
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-04-03 22:36:08 -0300

    Refactor ActiveStorage module inclusion and update Rswag UI configuration for endpoint

--- app/controllers/concerns/set_current.rb
+++ app/controllers/concerns/set_current.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-module ActiveStorage
-  module SetCurrent
-    extend ActiveSupport::Concern
+module SetCurrent
+  extend ActiveSupport::Concern
 
-    included do
-      before_action do
-        ActiveStorage::Current.url_options = {
-          protocol: request.protocol,
-          host: request.host,
-          port: request.port
-        }
-      end
+  included do
+    before_action do
+      ActiveStorage::Current.url_options = {
+        protocol: request.protocol,
+        host: request.host,
+        port: request.port
+      }
     end
   end
 end
